91bc8b6aeebc47d8f03f43050094fb4e8e7959fe
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-01-14 19:04:28 -0300

    New contact view done

--- asp-net-mvc/Controllers/ContatoController.cs
+++ asp-net-mvc/Controllers/ContatoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using asp_net_mvc.Context;
+using asp_net_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -20,6 +21,24 @@ namespace asp_net_mvc.Controllers
         {
             var contatos = _context.Contatos.ToList();
             return View(contatos);    
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contato);
         }
     }
 }
